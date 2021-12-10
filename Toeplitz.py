b890c6c1d953d6e9f4a674400c345694f519260e
arequi <sarahg.3545@gmail.com>
2021-12-10 14:13:15 -0700

    updated function parameters

--- Toeplitz.py
+++ Toeplitz.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from scipy.constants import *
-from scipy.linalg import toeplitz
+from scipy.linalg import toeplitz  # Why doesn't it like these imports? <-----
 
 class Toeplitz:
     def __init__(self):
@@ -16,28 +16,25 @@ class Toeplitz:
     data = np.array(z_data["z_powmeas"])[-1]
 
 
-    def calculate_N(self, data):
+    def calculate_N(self, data):  # What does self mean? <----- Is there another file that is calling these funcions? How does it work?
         N = 0 # Used to generate 2^N raw data points from normal distribution
-
         while (data.size - 2**N) >= 0:
             N += 1
         N -= 1
-
         indices = []
         for i in range(2**N, data.size):
             indices.append(i)
-
-        new_data = np.delete(data, indices)
-        return new_data
+        data = np.delete(data, indices)
+        return data
         
 
-    def plot_data(self, new_data, n): # added n parameter <-----
+    def plot_data(self, data, n):
         # Bin up voltages and assign each bin a number from 0-255
-        binned_data, bins = np.histogram(new_data, bins=2**n-1) 
-        data_digital = np.digitize(new_data, bins, right=True) # Digitize raw data
-
+        binned_data, bins = np.histogram(data, bins=2**n-1)       # Do we need to separate binned_data from plot_data functions? <-----
+        # Digitize raw data
+        data_digital = np.digitize(data, bins, right=True) 
         # Plot histogram of raw digitized data
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots()  # Where does fig come from? <--------
         ax.hist(data_digital,bins=2**n-1, label='Digitized Raw Data')
         ax.legend()
         plt.xlabel('Random numbers')
@@ -46,37 +43,28 @@ class Toeplitz:
         plt.show()
         return binned_data #fixme: idk if this is what we want to return <----- 
 
-    # Calculate signal to noise and quantum standard deviation block? <-----
-
-    def min_entropy(self, binned_data, N): # added 2 parameters <-----
+    def min_entropy(self, binned_data, N):
         # Find probability max
         pmax = np.max(binned_data)/2**N
-
         # Find minimum-entropy
         min_ent = -np.log2(pmax)
-        #quant_ent = min_ent * (-(1/(sig2noise + 1)) + 1)
-
-        # Calculate security parameter 
-        #secur_param = 2**(-100) # do we need to include security parameter? probably not for now? <-----
-
         return min_ent
 
     # Find output length
-    def output_length(self, n, min_ent): # added 2 params, deleted data param<-----
-        #m = min_ent - 2 * np.log2(secur_param)
-        m = 2**n * (min_ent/n)
-        m = round(m)
-        return m
+    def output_length(self, n, min_ent):
+        out_len = 2**n * (min_ent/n)
+        out_len = round(out_len)
+        return out_len
 
     # Generate random 2^n by 2^m toeplitz matrix
-    def toep_mat(self, out_len, n): # deleted data param <-----
+    def toep_mat(self, out_len, n):
         row = np.random.randint(2, size=out_len)
         col = np.random.randint(2, size=2**n)
         toep_mat = toeplitz(row, col)
         return toep_mat
 
     # Convert digitized raw data to binary
-    def decToBin_data(self, data): #fixme: params <-----
+    def decToBin_data(self, data_digital, N):
         def decToBin(data_pt, depth, bin_pts): # Where does bin_pts come from? <-----
             if data_pt >= 1:
                 bin_pts = decToBin(data_pt // 2, depth - 1, bin_pts)
@@ -85,44 +73,27 @@ class Toeplitz:
         binary_data = []
         for i in range(2**N):
             zeros = np.zeros(8)
-            binary_data.append(decToBin(data_digital[i], 7, zeros)) #where does data_digital come from? <-----
+            binary_data.append(decToBin(data_digital[i], 7, zeros))
         binary_data = np.reshape(binary_data, (2**N, 8))
         data_flat = binary_data.flatten()
-        return data_flat # fixme: return statement <-----
+        return data_flat
 
     # Toeplitz Hash function
-    def toeplitz_hash(self, data):
-        # tempN = 15
-
-        # Split data into smaller chunks of size 2^tempN so it doesn't time out
-        # chunks = np.array_split(data_flat, n * 2**(N-tempN))
-        # print(len(chunks))
-
-        # for i, data in enumerate(chunks):
+    def toeplitz_hash(self, data_flat, m, n, N, toep_mat):
         # Split digitized data into chunks of size 2^n
         split = np.array_split(data_flat, n * 2**(N-n)) 
-
         # perform matrix multiplication of Toeplitz with data chunks
         data_hashed = np.dot(toep_mat, split[0]) % 2
-        for index, data in enumerate(split[1:-1]):
-            sample_hashed = np.dot(toep_mat, data) % 2
+        for index, data in enumerate(split[1:-1]):  # What is index? <----- Should this data be data_flat? <-----
+            sample_hashed = np.dot(toep_mat, data) % 2  # Should this data be data_flat? <-----
             data_hashed = np.append(data_hashed, sample_hashed)
-            
-        # f = open("binary.txt", "w")
-        # for i in range(len(data_hashed)):
-        #     f.write(str(int(data_hashed[i])))
-        # f.close()
-
         # split hashed data into chunks of size 2^n
         data_hashed = np.array_split(data_hashed, m * 2**(N-n))
-        print(data_hashed[0:10])
-
         decimal = []
-        for index, sample in enumerate(data_hashed):
+        for index, sample in enumerate(data_hashed): # Index? Do we need it? <-----
             x = ''.join([str(int(elem)) for elem in sample])
             decimal = np.append(decimal, int(x, 2))
         decimal = decimal[decimal != 255]
-        pass #fixme: return statement
-
-    
+        return decimal #?? <-----
+   
 
