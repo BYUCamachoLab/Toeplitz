6003544249c47d2c6cb75289ad4b4187ead3dd14
arequi <sarahg.3545@gmail.com>
2022-01-05 12:08:52 -0700

    fixing parameters

--- Toeplitz.py
+++ Toeplitz.py
@@ -1,8 +1,9 @@
+from typing import Callable
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from scipy.constants import *
-from scipy.linalg import toeplitz  # Why doesn't it like these imports? <-----
+from scipy.linalg import toeplitz
 
 class Toeplitz:
     def __init__(self):
@@ -16,7 +17,7 @@ class Toeplitz:
     data = np.array(z_data["z_powmeas"])[-1]
 
 
-    def calculate_N(self, data):  # What does self mean? <----- Is there another file that is calling these funcions? How does it work?
+    def calculate_N(self, data):  
         N = 0 # Used to generate 2^N raw data points from normal distribution
         while (data.size - 2**N) >= 0:
             N += 1
@@ -25,47 +26,48 @@ class Toeplitz:
         for i in range(2**N, data.size):
             indices.append(i)
         data = np.delete(data, indices)
-        return data
+        return N, data
         
 
     def plot_data(self, data, n):
         # Bin up voltages and assign each bin a number from 0-255
-        binned_data, bins = np.histogram(data, bins=2**n-1)       # Do we need to separate binned_data from plot_data functions? <-----
+        binned_data, bins = np.histogram(data, bins=2**n-1)             #binned_data created
         # Digitize raw data
-        data_digital = np.digitize(data, bins, right=True) 
+        data_digital = np.digitize(data, bins, right=True)              #data_digital created
         # Plot histogram of raw digitized data
-        fig, ax = plt.subplots()  # Where does fig come from? <--------
+        fig, ax = plt.subplots()  
         ax.hist(data_digital,bins=2**n-1, label='Digitized Raw Data')
         ax.legend()
         plt.xlabel('Random numbers')
         plt.ylabel('Frequency')
         plt.title("-35 dbm measurements")
         plt.show()
-        return binned_data #fixme: idk if this is what we want to return <----- 
+        return binned_data, data_digital                               
 
-    def min_entropy(self, binned_data, N):
+    def min_entropy(self, binned_data):                                  
+        N, data = calculate_N(self, binned_data)
         # Find probability max
-        pmax = np.max(binned_data)/2**N
+        pmax = np.max(binned_data)/2**N                                  #call for binned_data
         # Find minimum-entropy
-        min_ent = -np.log2(pmax)
-        return min_ent
+        min_ent = -np.log2(pmax)                                         #min_ent created
+        return min_ent                                                
 
     # Find output length
-    def output_length(self, n, min_ent):
-        out_len = 2**n * (min_ent/n)
+    def output_length(self, n, min_ent):                                 #call for min_ent
+        out_len = 2**n * (min_ent/n)                                     #out_len created
         out_len = round(out_len)
-        return out_len
+        return out_len                                                 
 
     # Generate random 2^n by 2^m toeplitz matrix
     def toep_mat(self, out_len, n):
-        row = np.random.randint(2, size=out_len)
+        row = np.random.randint(2, size=out_len)                         #call for out_len
         col = np.random.randint(2, size=2**n)
-        toep_mat = toeplitz(row, col)
-        return toep_mat
+        toep_mat = toeplitz(row, col)                                    #toep_mat created
+        return toep_mat                                                 
 
     # Convert digitized raw data to binary
-    def decToBin_data(self, data_digital, N):
-        def decToBin(data_pt, depth, bin_pts): # Where does bin_pts come from? <-----
+    def decToBin_data(self, data_digital, N):                            #call for data_digital
+        def decToBin(data_pt, depth, bin_pts): 
             if data_pt >= 1:
                 bin_pts = decToBin(data_pt // 2, depth - 1, bin_pts)
                 bin_pts[depth] = data_pt % 2
@@ -73,27 +75,27 @@ class Toeplitz:
         binary_data = []
         for i in range(2**N):
             zeros = np.zeros(8)
-            binary_data.append(decToBin(data_digital[i], 7, zeros))
+            binary_data.append(decToBin(data_digital[i], 7, zeros))      #call for data_digital
         binary_data = np.reshape(binary_data, (2**N, 8))
-        data_flat = binary_data.flatten()
+        data_flat = binary_data.flatten()                                #data_flat created
         return data_flat
 
     # Toeplitz Hash function
     def toeplitz_hash(self, data_flat, m, n, N, toep_mat):
-        # Split digitized data into chunks of size 2^n
-        split = np.array_split(data_flat, n * 2**(N-n)) 
+        # Split digitized data into chunks of size 2^n                      #call for N
+        split = np.array_split(data_flat, n * 2**(N-n))                  #call for data_flat
         # perform matrix multiplication of Toeplitz with data chunks
         data_hashed = np.dot(toep_mat, split[0]) % 2
-        for index, data in enumerate(split[1:-1]):  # What is index? <----- Should this data be data_flat? <-----
-            sample_hashed = np.dot(toep_mat, data) % 2  # Should this data be data_flat? <-----
+        for index, data in enumerate(split[1:-1]):  
+            sample_hashed = np.dot(toep_mat, data) % 2 
             data_hashed = np.append(data_hashed, sample_hashed)
         # split hashed data into chunks of size 2^n
-        data_hashed = np.array_split(data_hashed, m * 2**(N-n))
-        decimal = []
-        for index, sample in enumerate(data_hashed): # Index? Do we need it? <-----
+        data_hashed = np.array_split(data_hashed, m * 2**(N-n))             #call for m?
+        decimal = []                                                        #decimal created
+        for index, sample in enumerate(data_hashed): 
             x = ''.join([str(int(elem)) for elem in sample])
             decimal = np.append(decimal, int(x, 2))
         decimal = decimal[decimal != 255]
-        return decimal #?? <-----
+        return decimal 
    
 
